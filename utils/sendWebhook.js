deedc511692b9063d96cc7d579b97d81a01b1f77
easytypeneocitiesorg-lgtm <easytype.neocities.org@gmail.com>
2025-12-03 21:59:19 -0600

    Update sendWebhook.js

--- utils/sendWebhook.js
+++ utils/sendWebhook.js
@@ -1,4 +1,4 @@
-export async function sendToWebhook(stickerUrl) {
+export async function sendToWebhook(data) {
   try {
     const webhook = process.env.WEBHOOK_URL;
     if (!webhook) {
@@ -11,9 +11,7 @@ export async function sendToWebhook(stickerUrl) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        content: stickerUrl,
-      }),
+      body: JSON.stringify(data),
     });
   } catch (err) {
     console.error("Error sending to webhook:", err);
